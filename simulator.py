f259331147c770b97549df755b9174564931f18b
Alex B. Jose <ajose@caltech.edu>
2014-04-12 00:33:16 -0700

    add initial simulation code

--- simulator.py
+++ simulator.py
@@ -14,8 +14,26 @@ Have a bag of agents, facilitate interactions...
 class Simulator:
 
     def __init__(self, ):
-        pass
+        self.agents = []
 
+    def tick(self, ):
+        # shuffle agents
+        np.random.shuffle(self.agents)
+        # keep track of who's still living
+        alive = self.agents
+        # then iterate through and act
+        for a in self.agents:
+            a.tick(alive)
+            if a.food <= 0 or a.leisure <= 0:
+                # kill a
+                print 'WOE IS ME!'
+                alive.remove(a)
+        # remove dead agents from original list
+        self.agents = alive
+
+    def run(self, ):
+        while True:
+            self.tick()
 
     # in whichever function makes agents act, make sure to remove
     # agent from list if it dies
